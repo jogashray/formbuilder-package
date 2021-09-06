fbcada8847f9ad4b80fa0af5dbb8ced8799c420f
jogashray <jogashbrur.ete@gmail.com>
2021-09-06 08:08:47 +0600

    Remove Unnesecessary Files

--- src/FormBuilderServiceProvider.php
+++ src/FormBuilderServiceProvider.php
@@ -5,7 +5,6 @@ use Illuminate\Support\ServiceProvider;
 class FormBuilderServiceProvider extends ServiceProvider
 {
     public function boot(){
-        $this->loadRoutesFrom(__DIR__.'./routes/web.php');
 
     }
     public function register(){
